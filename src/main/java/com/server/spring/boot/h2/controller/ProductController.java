125ed1a75eedfce94a89b480ee9d795d8f549bb7
AWOKOU Komivi <komivi.awokou@ext.soprasteria.com>
2025-09-04 21:02:17 +0200

    Initial project

--- src/main/java/com/server/spring/boot/h2/controller/ProductController.java
+++ src/main/java/com/server/spring/boot/h2/controller/ProductController.java
@@ -9,14 +9,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@Tag(name = "Product", description = "Product management APIs")
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class ProductController {
